2402f9f54821d1440fec87c905a35fbc073be75e
AnirudhaKhasnobis <125763297+AnirudhaKhasnobis@users.noreply.github.com>
2024-04-01 15:47:25 +0530

    5th commit

--- script.js
+++ script.js
@@ -9,6 +9,47 @@ function getWeather(){
 
     const currentWeatherUrl = 'https://api.openweathermap.org/ data/2.5/weather?q=${city}&appid=${apiKey}`;
     const forecastUrl = 'https://api.openweathermap.org/data/ 2.5/forecast?q=${city}&appid=${apiKey}`;     
+
+
+
+
+    fetch(currentWeatherUrl)
+    .then(response => response.json())
+    .then(data => {displayWeather(data);})
+    .catch(error => {console.error('Error fetching current weather data:', error);
+    alert ('Error fetching current weather data. Please try again.');});
+
+    fetch(forecastUrl)
+    .then(response => response.json())
+    .then(data => {displayHourlyForecast(data.list);})
+    .catch(error => {console.error('Error fetching hourly forecast data:', error);
+    alert ('Error fetching hourly forecast data. Please try again.');});
+}
+
+function displayWeather(data){
+    const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+
+//clears previous content
+    weatherInfoDiv.innerHTML = '';
+    hourlyForecastDiv.innerHTML = '';
+    tempDivInfo.innerHTML = '';   
+
+    if (data.cod === '404'){
+        weatherInfoDiv.innerHTML = '<p>${data.message}</p>';
+    }    
+    else{
+
+        const cityname = data.name;
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;        
+        const iconUrl = 'https://openweathermap.org/img/wn/${iconCode @4x.png';
+
+    }
+
 }
 
 
