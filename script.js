62eaccb80d24bdbdf537fb45461ae2476a00736c
AnirudhaKhasnobis <125763297+AnirudhaKhasnobis@users.noreply.github.com>
2024-04-01 10:30:51 +0530

    5th Commit

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ function getWeather(){
     const apiKey = 'dd0200d0aaa5c997c671ab248e49baf8';
     const city = document.getElementById('city').value;
 
+    if(!city){
+        alert('Please enter a city');
+        return;
+    }
+
+    const currentWeatherUrl = 'https://api.openweathermap.org/ data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const forecastUrl = 'https://api.openweathermap.org/data/ 2.5/forecast?q=${city}&appid=${apiKey}`;     
 }
+
+
